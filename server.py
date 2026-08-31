6865f7e97eaced3e3f2e602f2fc4b397a6ca3f5a
warmheartsministries <92443398+warmheartsministries@users.noreply.github.com>
2026-08-31 19:41:29 -0400

    modified server.py for pylint

--- server.py
+++ server.py
@@ -1,3 +1,8 @@
+"""
+This is a Flask server for the Emotion Detector application.
+It has endpoints that accepts text and outputs emotions scores.
+"""
+
 from flask import Flask, render_template, request
 from EmotionDetection.emotion_detection import emotion_detector
 
@@ -5,6 +10,11 @@ app = Flask("Emotion Detector")
 
 @app.route("/emotionDetector")
 def emo_detector():
+    """
+    Retrieves the text to analyze and processes it using the emotion
+    detector function. It returns a JSON payload which contains the
+    emotions score and the dominant emotion.
+    """
     # Retrieve the emotion text to analyze from the request arguments
     text_to_analyze = request.args.get('textToAnalyze')
     print(text_to_analyze)
@@ -15,13 +25,19 @@ def emo_detector():
     # Check if the emotions is None, indicating an error or invalid input
     if response['dominant_emotion'] is None:
         return "Invalid text! Try again."
-    else:
-        emotions_text = f"'anger': {response['anger']}, 'disgust': {response['disgust']}, 'fear': {response['fear']}, 'joy': {response['joy']}, 'sadness': {response['sadness']}"
-        # Return the emotion predictions with the scores
-        return f"For the given statement, the system response is {emotions_text}. The dominant emotion is {response['dominant_emotion']}."
+    keys = ['anger', 'disgust', 'fear', 'joy', 'sadness']
+    emotions_text = emotions_text = ", ".join(f"'{k}': {response[k]}" for k in keys)
+    # Return the emotion predictions with the scores
+    return(
+        f"For the given statement, the system response is {emotions_text}."
+        f"The dominant emotion is {response['dominant_emotion']}."
+    )
 
 @app.route("/")
 def render_index_page():
+    """
+    Retrieves the index page.
+    """
     return render_template('index.html')
 
 if __name__ == "__main__":
